190775f9fb944872c32025a129becbadd926d1d3
Kanachiw <101941959+Kanachiw@users.noreply.github.com>
2022-03-19 13:39:45 -0400

    Create isiagu.py

---
+++ isiagu.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 19 10:35:53 2022
+
+@author: kanachiweli
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar 11 02:33:32 2022
+
+@author: kanachiweli
+"""
+
+def intro():
+        key= input("Press c to continue and any other key to exit _\n")
+        if (key != "c"):
+            print("\n I understand you would like to browse on your own today\nI will be on the bottom right corner if you need any assistance")
+            return 0
+           
+        else:
+            Num=int(input("\n\033[0mMain Menu: Our app is founded on the 3 Pillars \033[0m\n\
+\n\033[0mPress 1 to explore Product:\033[0m Online Shop through our Fashion catalog that samples\
+       \t fashion styles throughout the diaspora\n\
+\033[0mPress 2 to explore Podcast:\033[0m listen to tailored podcast focused on elevating black\
+        \tvoices\
+\n\033[0mPress 3 to explore Promise:\033[0m Our promise to raise fund to provide scholarships and\
+       \t training workshops for Black/Carribean/African students"))
+           
+   
+     
+
+intro()
